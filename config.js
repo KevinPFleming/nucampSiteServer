c4cb7e2dd5bf2638bc1ac053a585f40a470b35eb
Kevin P. Fleming <kpfguitar77@gmail.com>
2021-07-08 11:10:38 -0400

    Facebook Passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2275302729273698',
+        clientSecret: '5cb27a38aeb6b88e6ca06ede928778ba'
+    }
 }
